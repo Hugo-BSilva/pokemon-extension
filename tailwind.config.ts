0faf5f92a86ae008a6d925ebf947e8fc2cf2bdbc
Hugo Barbosa da Silva <70847074+Hugo-BSilva@users.noreply.github.com>
2025-09-08 21:58:11 -0300

    refactor: continuation of refactoring

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+// import { defineConfig } from 'tailwindcss'
+
+// export default defineConfig({
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         brand: "#1DA1F2"
+//       }
+//     },
+//   },
+//   plugins: [],
+// })
+
+// @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
